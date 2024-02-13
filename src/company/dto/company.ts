f395576149dc144cd8757cfcc7c6c9d11e2293f7
Vindo Kumar <vinodB@sparkgenieit.com>
2024-02-13 20:08:07 +0530

    send

---
+++ src/company/dto/company.ts
@@ -0,0 +1,14 @@
+ export class Company{
+    user_id: string
+    name: string
+    address1: string
+    address2: string
+    address3: string
+    city: string
+    postalCode: string
+    email: string
+    contact: string
+    website: string
+    logo: string
+}
+
