b59826ee6508c1d9f0044c543453d7d0a917e16b
kiranfstack <kirankumarsabapathi@gmail.com>
2024-03-21 19:59:49 +0530

    api chageS

--- src/jobs/jobs.module.ts
+++ src/jobs/jobs.module.ts
@@ -6,15 +6,17 @@ import { JwtService } from '@nestjs/jwt';
 import { Jobs, JobsSchema } from './schema/Jobs.schema';
 import { JobsService } from './jobs.service';
 import { User, UserSchema } from 'src/users/schema/user.schema';
+import { UserJobs, UserJobsSchema } from 'src/users/schema/userJobs.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Jobs.name, schema: JobsSchema },
-      { name: User.name, schema: UserSchema }
+      { name: User.name, schema: UserSchema },
+      { name: UserJobs.name, schema: UserJobsSchema}
     ])
   ],
-  providers: [Jobs, JobsService, User, JwtService],
+  providers: [Jobs, JobsService, User, UserJobs, JwtService],
   controllers: [jobsController]
 })
 export class JobsModule {}
