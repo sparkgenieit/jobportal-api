a44a36404b24399629a8c245ba830dea638fb9ca
sparkgenieit <sparkgenieit@gmail.com>
2024-04-05 20:53:47 +0530

    MIssed app module skill adding

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AuthModule } from './auth/auth.module';
 import { CompanyModule } from './company/company.module';
 import { JobsModule } from './jobs/jobs.module';
 import { AdModule } from './ads/ad.module';
+import { SkillModule } from './skills/skill.module';
 import { UploadModule } from './upload/upload.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
@@ -15,7 +16,7 @@ import { join } from 'path';
 @Module({
   imports: [ServeStaticModule.forRoot({
     rootPath: join(__dirname, '..', 'public'),
-}), DatabaseModule, UsersModule, CompanyModule, JobsModule, AuthModule, UploadModule, AdModule],
+}), DatabaseModule, UsersModule, CompanyModule, JobsModule, AuthModule, UploadModule, AdModule, SkillModule],
   controllers: [AppController],
   providers: [AppService],
 })
